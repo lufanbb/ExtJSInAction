9e89c7f1a7c32c16d670a9969b44f7008e574930
lufanbb <lufanlucas@gmail.com>
2016-05-03 13:31:33 -0500

    1. Remove Nodes from Div using Ext.Element.remove 2. Ext Js Ajax call using Ext.Element.load()

---
+++ RemoveNodes.js
@@ -0,0 +1,15 @@
+function removeChildFromSelector(){
+	var myDiv1 = Ext.get('div1');
+	var lastChild = myDiv1.down('div:last-child');
+	lastChild.remove();
+}
+function removeChildFromChaining(){
+	Ext.get('child4').remove();
+}
+
+Ext.onReady(removeChildFromSelector);
+Ext.onReady(removeChildFromChaining);
+Ext.getBody().load({
+	url		: 'htmlFragment.html',
+	scripts	: true
+});
