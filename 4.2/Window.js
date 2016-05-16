37ac95726beb7e19856b3de11f4a4891151654d9
Lu <flu@elogex.com>
2016-05-16 17:25:51 -0500

    4.2. Add new Window that is not resizable.

--- 4.2/Window.js
+++ 4.2/Window.js
@@ -26,3 +26,24 @@ new Ext.Button({
 	style: 'margin: 100px',
 	handler: newWindow
 });
+
+var win2 = new Ext.Window({
+	height: 75,
+	width: 200,
+	modal: true,
+	title: 'This is one rigid window',
+	html: 'Try to move or resize me. I dare you.',
+	plain: true,
+	border: false,
+	resizable: false,
+	draggable: false,
+	closable: false,
+	buttonAlign: 'center',
+	buttons: [{
+		text: 'I give up!',
+		handler: function() {
+			win2.close();
+		}
+	}]
+})
+win2.show();
