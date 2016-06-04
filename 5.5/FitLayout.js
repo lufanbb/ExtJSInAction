4b7d4327f265c18cdba2d5f068859a900e8c7f65
flu <lufan1988@gmail.com>
2016-06-03 21:49:40 -0500

    5.4 AbsoluteLayout 5.5 FitLayout 5.6 ArccordionLayout 5.7 CardLayout

---
+++ 5.5/FitLayout.js
@@ -0,0 +1,12 @@
+var myWin = new Ext.Window({
+	height: 200,
+	width: 200,
+	layout: 'fit',
+	border: false,
+	items: [{
+		title: 'Panel1',
+		html: 'I fit in my parent!',
+		frame: true
+	}]
+});
+myWin.show();
